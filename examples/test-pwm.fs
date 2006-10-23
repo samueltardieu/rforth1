f0b7b2a3c9dafa0a53b72e0fb3369f57d6c6b996
Samuel Tardieu <sam@rfc1149.net>
2006-10-23 23:06:40 +0200

    Cleanup PWM generator example

--- examples/test-pwm.fs
+++ examples/test-pwm.fs
@@ -1,60 +1,50 @@
 needs lib/tty-rs232.fs
 
--25000 constant main-time
-
 300 constant delta
 
 variable pulse0
 
-: set-tmr0-wait ( n -- ) TMR0L ! TMR0IF bit-clr begin TMR0IF bit-set? until ;
+LATB 5 bit led0
+LATB 6 bit led1
 
-: tmr1-wait ( -- ) begin TMR1IF bit-set? until ;
+: reset-tmr0 ( -- ) pulse0 @ TMR0L ! TMR0IF bit-clr ;
+: wait-for-tmr0 ( -- ) begin TMR0IF bit-set? until ;
+: tmr0-cycle ( -- ) reset-tmr0 wait-for-tmr0 ;
 
-: set-tmr1 ( n -- ) TMR1L ! TMR1IF bit-clr ;
+: reset-tmr1 ( n -- ) -25000 TMR1L ! TMR1IF bit-clr ;
 
-: pwm 
-	begin
-  	main-time set-tmr1 
-        LATB 5 bit-set pulse0 @ set-tmr0-wait LATB 5 bit-clr
-	( pulse-max set-tmr1 )
-	begin RCIF bit-set? if exit then TMR1IF bit-set? until
-
-        again
+: pwm  ( -- )
+  begin
+    reset-tmr1
+    led0 bit-set tmr0-cycle led0 bit-clr
+    begin key? if exit then TMR1IF bit-set? until
+  again
 ;
 
-: main-loop ( -- )
-	-25000 pulse0 !
-	
-	begin
-	pwm
-        key 
-	dup [char] 0 = if
-	    read16 pulse0 ! 
-		." 0"
-	then
-	dup [char] p = if
-		." PWM 0 : " pulse0 @ . cr
-	then
-	dup	[char] + = if
-		pulse0 @ delta + pulse0 !
-		." +" pulse0 @ . cr
-	then
-	dup	[char] - = if
-		pulse0 @ delta - pulse0 !
-		." -" pulse0 @ . cr
-	then
-        dup [char] d = if LATA 2 bit-toggle then
-	drop
-    again ;
+: .pulse0 ( -- ) pulse0 @ . cr ;
 
-: greetings ( -- )
-  ." \nPWM Generator>\n" ;
+: main-loop ( -- )
+  -25000 pulse0 !
+  begin
+    pwm
+    key >w switchw
+      [char] 0 casew read16 pulse0 ! [char] 0 emit
+      [char] p casew ." PWM 0 : " .pulse0
+      [char] + casew pulse0 @ delta + pulse0 ! [char] + emit .pulse0
+      [char] - casew pulse0 @ delta - pulse0 ! [char] - emit .pulse0
+      [char] d casew led1 bit-toggle
+      [char] q casew exit
+    endswitchw
+  again
+;
+
+: greetings ( -- ) cr ." PWM Generator>" cr ;
 
 : init-pwm ( -- )
   7 ADCON1 c!
   TRISB 5 bit-clr
-  TRISC 5 bit-clr
-  0b10000001 T0CON c! \ pas de prescaler
+  TRISB 6 bit-clr
+  0b10000001 T0CON c! \ no prescaler
   0 TMR1H c! 0 TMR1L c!
   0b10110001 T1CON c! \ prescaler = 8
 ;
