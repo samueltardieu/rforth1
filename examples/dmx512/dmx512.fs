b5aa8df6bd9c343d391fe15a80de2529958bc1cf
Samuel Tardieu <sam@rfc1149.net>
2007-10-15 14:30:23 +0200

    Code cleanup

--- examples/dmx512/dmx512.fs
+++ examples/dmx512/dmx512.fs
@@ -17,12 +17,12 @@
 
 needs lib/tty-rs232.fs
 
-variable highest
-variable index
-variable current
-cvariable step
-variable assignment-device
-cvariable assignment-value
+variable highest                   \ Highest channel used so far
+variable index                     \ Address of the channel being sent
+variable current                   \ Latest transmitted channel
+cvariable state                    \ Current state of the state machine
+variable assignment-device         \ Channel to change (state machine)
+cvariable assignment-value         \ Value to set to channel (state machine)
 
 \ It is best to create this large area in the last position to allow
 \ other variables to be placed in bank 1 which is addressable through
@@ -30,10 +30,14 @@ cvariable assignment-value
 \ be a net gain.
 create channels 512 allot
 
-PORTB 0 bit DMXOUT
+PORTB 0 bit DMXOUT                 \ Shortcut to the port to use
 
-: set-channel ( data channel -- ) channels 1 - + c! ;
+\ Set a channel to a given value
+: set-channel ( value channel -- ) channels 1- + c! ;
 
+\ Set all channels to zero -- note that the loop can count 255 times as
+\ a maximum, so channels 1 (stored as 0) and 257 (stored as 1) must
+\ be set explicitely. Also, mark every channel as unset yet.
 : reset-channels ( -- )
   255 cfor 0 cr@ 2dup set-channel 256 + set-channel cnext
   0 0 set-channel
@@ -41,67 +45,72 @@ PORTB 0 bit DMXOUT
   0 highest !
 ;
 
+\ Channel 0 is a virtual channel used to send commands to this program
 : set-channel-0 ( data -- )
   0= if reset-channels then ;
 
-: reset-step ( -- ) 0 step c! ;
+\ Reset state machine
+: reset-state ( -- ) 0 state c! ;
 
-: advance-step ( -- ) 1 step c+! ;
+\ Go to next state
+: advance-state ( -- ) 1 state c+! ;
 
-: handle-step-0 ( char -- ) [CHAR] ! = if advance-step then ;
+\ State 0: wait for '!'
+: handle-state-0 ( char -- ) [CHAR] ! = if advance-state then ;
 
-: handle-step-1 ( char -- )
+\ State 1: wait for high byte of channel address and check for address validity
+: handle-state-1 ( char -- )
   switchw
-    0 casew 0 assignment-device ! advance-step
-    1 casew 256 assignment-device ! advance-step
-    2 casew 512 assignment-device ! advance-step
-    defaultw reset-step
+    0 casew 0 assignment-device ! advance-state
+    1 casew 256 assignment-device ! advance-state
+    2 casew 512 assignment-device ! advance-state
+    defaultw reset-state
   endswitchw
 ;
 
-: handle-step-2 ( char -- )
+\ State 2: wait for low byte of channel address and check address validity
+: handle-state-2 ( char -- )
   assignment-device +!
-  assignment-device c@ 512 <= if advance-step else reset-step then
+  assignment-device c@ 512 <= if advance-state else reset-state then
 ;
 
-: handle-step-3 ( char -- ) assignment-value c! advance-step ;
+\ State 3: read value to assign to previously read channel
+: handle-state-3 ( char -- ) assignment-value c! advance-state ;
 
-: handle-step-4 ( char -- )
-  [CHAR] D <> if reset-step exit then  
+\ State 4: wait for 'D', make assignment and mark channel as used
+: handle-state-4 ( char -- )
+  [CHAR] D <> if reset-state exit then  
   assignment-value c@ assignment-device @
   dup 0= if drop set-channel-0 else set-channel then
   assignment-device @ highest @ > if
     assignment-device @ highest !
   then
-  reset-step
+  reset-state
 ;
 
-: handle-step ( char -- )
-  step c@
+\ State machine: depending on the current state, handle incoming character
+: handle-state ( char -- )
+  state c@
   switchw
-    0 casew handle-step-0
-    1 casew handle-step-1
-    2 casew handle-step-2
-    3 casew handle-step-3
-    4 casew handle-step-4
+    0 casew handle-state-0
+    1 casew handle-state-1
+    2 casew handle-state-2
+    3 casew handle-state-3
+    4 casew handle-state-4
   endswitchw
 ;
 
-: handle-serial-port ( -- )
-  key? 0= if exit then
-  key handle-step
-;
+\ If a character is present on the serial port, handle it through the state
+\ machine
+: handle-serial-port ( -- ) key? if key handle-state then ;
 
+\ Start transmission by transmitting the 32 first channels in any case
 : reset-transmission ( -- ) channels index ! 32 current ! ;
-
-: set-output ( b -- ) if DMXOUT bit-set else DMXOUT bit-clr then ;
 
 \ Set output line according to low bit of W and shift it right in 10 cycles
 \ 8 cycles are used before bit is set on the line and 2 after for return
-
 : set-zero ( -- ) nop DMXOUT bit-clr ; no-inline
 : set-one ( -- ) DMXOUT bit-set ; no-inline
-
 code set-bit
   WREG ,w ,a rrcf
   C ,a btfss
@@ -111,35 +120,34 @@ code set-bit
 ;code no-inline
 
 \ Wait for 28 cycles (2 to call, 2 to return, 24 nop)
-  
 : nop28 ( -- )
   nop nop nop nop nop nop nop nop nop nop nop nop
   nop nop nop nop nop nop nop nop nop nop nop nop
 ; no-inline
 
 \ Wait for 29 cycles (nop28, 1 nop)
-
 : nop29 ( -- ) nop28 nop ; inline
 
 \ Wait for 30 cycles (nop29, 1 nop)
-
 : nop30 ( -- ) nop29 nop ; inline
 
 \ Wait for 36 cycles (2 to call, 2 to return, nop30, 2 nop)
-
 : nop36 ( -- )
   nop30 nop nop
 ; no-inline
 
+\ Set a low or high state on the DMX line and wait for an approximate
+\ time (no need to be precise here)
 : transmit-zero ( -- ) 0 >w set-bit nop36 ; no-inline
 : transmit-one ( -- ) 255 >w set-bit nop36 ; no-inline
-  
-: transmit-break ( -- ) 25 cfor transmit-zero cnext ; \ Required: 22
 
-: transmit-mab ( -- ) transmit-one transmit-one ; \ Mark after break
-
+\ Control the line -- no need to be precise
+: transmit-break ( -- ) 25 cfor transmit-zero cnext ; \ Required: at least 22
+: transmit-mab ( -- ) transmit-one transmit-one ;     \ Mark after break
 : transmit-stop ( -- ) transmit-mab ;
 
+\ Transmit byte with a very precise timing (40 cycles at 40MHz, 4 clock
+\ cycles per instruction, gives the expected timing for 250kbps)
 : transmit-byte ( b -- )
   0 >w set-bit   \ 2 cycles elapsed since set
   nop28
@@ -165,16 +173,22 @@ code set-bit
   transmit-stop
 ;
 
+\ Transmit current channel
 : transmit-current ( -- ) index @ dup c@ transmit-byte 1+ index ! ;
-  
+
+\ Transmit the next 32 channels
 : transmit-32 ( -- ) 32 cfor transmit-current cnext ;
 
+\ Transmit the next 32 channels if they have been used
 : transmit-more ( -- f )
   current @ highest @ >= if 0 exit then
   transmit-32
   current @ 32 + current ! 1 exit
 ;
 
+\ Transmit some of the channels onto the DMX bus -- at least 32 channels
+\ must be transmitted anyway (some devices seem to hang if less than 24
+\ channels are transmitted in a DMX packet), so group them by 32
 : transmit-some ( -- )
   transmit-32
   transmit-more 0= if exit then
@@ -194,6 +208,7 @@ code set-bit
   transmit-more drop
 ;
 
+\ Send a whole DMX packet
 : transmit-all ( -- )
   reset-transmission
   transmit-break
@@ -202,13 +217,16 @@ code set-bit
   transmit-some
 ;
 
+\ Initial conditions
 : init ( -- )
-  1 set-output              \ idle state
-  0 step c!                 \ reset serial port state
+  TRISB 0 bit-clr           \ PORTB0 as output
+  transmit-one              \ Idle state, line must be set to high level
+  0 state c!                \ Reset state machine
   reset-channels
 ;
 
-: mainloop ( -- )
-  begin transmit-all again ;
+\ Main loop: transmit indefinitely
+: mainloop ( -- ) begin transmit-all again ;
 
+\ Main program
 : main ( -- ) init mainloop ;
