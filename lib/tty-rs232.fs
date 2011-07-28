c0508e267dc87561d7c40a6fea466c3c8d86db2f
Samuel Tardieu <sam@rfc1149.net>
2011-07-28 23:21:39 +0200

    Add tty-initialize for non-default tty initialization
    Closes #5.

--- lib/tty-rs232.fs
+++ lib/tty-rs232.fs
@@ -1,6 +1,7 @@
 \ Serial port handling
 
-: tty-init ( -- ) 0b00100000 TXSTA c! 64 SPBRG c! TRISC 6 bit-clr ;
+: tty-initialize ( spbrg/w -- ) w> SPBRG c! 0b00100000 TXSTA c! TRISC 6 bit-clr ; inw
+: tty-init ( -- ) 64 tty-initialize ;
 : emit ( c/w -- ) begin TXIF bit-set? until w> TXREG c! ; inw
 : key? ( -- f ) RCIF bit-set? ; inline
 : key ( -- c ) begin key? until RCREG c@ >w ; outw
