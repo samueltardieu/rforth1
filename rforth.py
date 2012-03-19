fad03dd67afbea0b7b3ad9d7c29cb7b650b4cfb7
Samuel Tardieu <sam@rfc1149.net>
2012-03-19 23:23:40 +0100

    Update copyright notice

--- rforth.py
+++ rforth.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 #
-# (c) 2005-2011 Samuel Tardieu <sam@rfc1149.net>
+# (c) 2005-2012 Samuel Tardieu <sam@rfc1149.net>
 #
 # rforth1 is released under the GNU General Public License (see the
 # file COPYING in this directory)
